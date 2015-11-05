9b345e1e41023d2edfc11cb622f398c3fea3cc86
Kindy Chen <517905407@qq.com>
2015-11-05 13:22:32 +0800

    Android for http and https

---
+++ src/com/kindy/httpurlconnection/MainActivity.java
@@ -0,0 +1,59 @@
+package com.kindy.httpurlconnection;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.kindy.httpurlconnection.utils.HttpUtils;
+
+public class MainActivity extends Activity {
+	
+	private ImageView img;
+	private Bitmap bmp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		img = (ImageView) findViewById(R.id.img);
+	}
+	
+	public void click(View view) {
+		System.out.println(this.getClass().getName() + " ================= click");
+		
+		new Thread() {
+			public void run() {
+				
+				String  url = "https://www.baidu.com/img/bd_logo1.png";
+				InputStream is = HttpUtils.getInstance().getInputStream(url);
+				if(is != null) {
+					bmp = BitmapFactory.decodeStream(is);
+					try {
+						is.close();
+					} catch (IOException e) { }
+				}
+				
+				runOnUiThread(new Runnable() {
+					public void run() {
+						if(bmp == null) {
+							img.setImageResource(R.drawable.ic_launcher);
+						} else {
+							img.setImageBitmap(bmp);
+						}
+						
+						bmp = null;
+					}
+				});
+				
+			}
+		}.start();
+	}
+}
